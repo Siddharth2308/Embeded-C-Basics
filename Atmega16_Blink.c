62b8d2c6f522ad6284c1d82dba226b76ae32ae41
Siddharth Kulkarni <45497245+Siddharth2308@users.noreply.github.com>
2020-12-18 11:51:04 +0530

    Update Atmega16_Blink.c

--- Atmega16_Blink.c
+++ Atmega16_Blink.c
@@ -3,6 +3,7 @@
  *
  * Created: 14-12-2020 18:57:34
  * Author : Siddharth
+ * Task   : Blinking LED Patterns.
  */ 
 #define  F_CPU 8000000UL
 #include <avr/io.h>
@@ -10,93 +11,69 @@
 
 int main(void)
 {
-	DDRB = 0xFF;
+	DDRB = 0xFF; //0b11111111// Sets All pins on portB to Output.
 	int temp;
-	int blink_Arr[4] = {0x81,0x42,0x24,0x18};
+	int blink_mid[4] = {0x81,0x42,0x24,0x18};
+	int blink_reverse[8] = {0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80};
 	
 	while(1){
-		for (int i= 3; i >= 0; i--)
-		{
-			temp = blink_Arr[i];
-			PORTB = temp;
-			_delay_ms(500);
-		}
-	}
-
-/*	Reverse Single	
-	DDRB = 0xFF;
-	int temp;
-	int blink_Arr[8] = {0x1,0x2,0x4,0x8,0x10,0x20,0x40,0x80};
+		//Full blink Code
 		
-	while(1){
-		for (int i = 7; i >= 0; i--)
-		{
-			temp = blink_Arr[i];
-			PORTB = temp;
-			_delay_ms(500);
-		}
-	}*/
-/*	Single Blink Start to End
-	
-	DDRB = 0xFF;
-	int temp;
-	int blink_Arr[8] = {0x1,0x2,0x4,0x8,0x10,0x20,0x40,0x80};
-	
-	while(1){
-		for (int i= 0; i< 8; i++)
-		{
-			temp = blink_Arr[i];
-			PORTB = temp;
-			_delay_ms(500);
-		}
-	}*/
-/*  End 2 Mid
-
-	DDRB = 0xFF;
-	int temp;
-	int blink_Arr[4] = {0x81,0x42,0x24,0x18};
-	
-	while(1){
-		for (int i= 0; i< 4; i++)
-		{
-			temp = blink_Arr[i];
-			PORTB = temp;
-			_delay_ms(500);
-		}
-	}*/
-/*	Mid 2 End Blink
-
-	DDRB = 0xFF;
-	int temp;
-	int blink_Arr[4] = {0x81,0x42,0x24,0x18};
-	
-	while(1){
-		for (int i= 3; i >= 0; i--)
-		{
-			temp = blink_Arr[i];
-			PORTB = temp;
-			_delay_ms(500);
-		}
-	}
-*/
-
-/*	ALternate Blink
-	DDRB = 0xFF;
-	while(1){
-		PORTB = 0x55;
+		PORTB = 0xFF;//Turns all pins on port B to high.
 		_delay_ms(500);
-		PORTB = 0xAA;
+		PORTB = 0x00;//Turns all pins on port B to Low.
+		_delay_ms(1000);
+		
+		//Alternate Blink
+		
+		PORTB = 0x55;// Turns alternate pins high. //0b00101010
 		_delay_ms(500);
-	}*/
-
-/*	Full blink Code
-
-	DDRB = 0xFF; //0b11111111//
-	
-	while(1){
-		PORTB = 0xFF;
+		PORTB = 0x00;//Turns All pins low
+		_delay_ms(500);
+		PORTB = 0xAA;// Vice versa
 		_delay_ms(500);
 		PORTB = 0x00;
 		_delay_ms(500);
-	}*/
+		
+		//Reverse Single
+		
+		for (int i = 7; i >= 0; i--)
+		{
+			temp = blink_reverse[i];
+			PORTB = temp;
+			_delay_ms(500);			
+		}
+		PORTB = 0x00;
+		_delay_ms(1500);
+		
+		// Top To Bottom Single
+		
+		for (int i = 0; i < 8; i++){
+			temp = blink_reverse[i];
+			PORTB = temp;
+			_delay_ms(500);
+		}
+		PORTB = 0x00;
+		_delay_ms(1500);
+		
+		// End to Mid
+		
+		for (int i = 0; i < 4; i++){
+			temp = blink_mid[i];
+			PORTB = temp;
+			_delay_ms(500);
+		}
+		PORTB = 0x00;
+		_delay_ms(1500);
+		
+		//Mid to End
+		
+		for (int i= 3; i >= 0; i--)
+		{
+			temp = blink_mid[i];
+			PORTB = temp;
+			_delay_ms(500);
+		}
+	}
 }
+
