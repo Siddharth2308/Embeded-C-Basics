ac9ef798275d103c532d4139fa3281160a1b1b02
Siddharth Kulkarni <45497245+Siddharth2308@users.noreply.github.com>
2020-12-18 13:09:27 +0530

    Blink patterns using atmega328p

---
+++ main.c
@@ -0,0 +1,67 @@
+/*
+ * Atmega328p-Bit-Blink.c
+ * Author : Siddharth
+ */ 
+
+#define  F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#define  BLINK_MS 150
+
+int main(void)
+{
+	//DDRB |= 0x20;//0b00100000; This is a trial to set a particular bit to high
+	//DDRB |= _BV(PORTB) - Alternative method
+	DDRB |= 0xFF; //ALl pins are intentionally set to high to test out patterns(Don't really need a bitwise or hear)
+	int temp;
+	int blink_mid[4] = {0x81,0x42,0x24,0x18};
+	int blink_reverse[8] = {0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80};
+
+    while (1) 
+    {
+		//Full blink code
+		
+		PORTB ^= 0xFF;//instead of on/off a  toggle logic
+		_delay_ms(BLINK_MS);
+
+		//Reverse single
+		PORTB = 0x00;
+		for (int i = 7; i >= 0; i--)
+		{
+			temp = blink_reverse[i];
+			PORTB |= temp;
+			_delay_ms(15);
+		}
+
+		// Top to bottom Single
+		PORTB = 0x00;
+		for (int i = 0; i < 8; i++){
+			temp = blink_reverse[i];
+			PORTB |= temp;
+			_delay_ms(15);
+		}
+		//Mid to end
+		
+		for (int i= 3; i >= 0; i--)
+		{
+			temp = blink_mid[i];
+			PORTB |= temp;
+			_delay_ms(50);
+		}
+		
+		//End to Mid
+		
+		for (int i = 0; i < 4; i++){
+			temp = blink_mid[i];
+			PORTB |= temp;
+			_delay_ms(50);
+		}
+						
+		// Alternate Blink
+				
+		PORTB ^= 0x55; //0b01010100
+		_delay_ms(BLINK_MS);
+				
+    }
+}
+
