aacd974270d69554013ae9485ce32bcf477adcfa
Siddharth Kulkarni <45497245+Siddharth2308@users.noreply.github.com>
2021-01-06 22:59:14 +0530

    Update Atmega16-PWM.c

--- Atmega16-PWM.c
+++ Atmega16-PWM.c
@@ -16,33 +16,31 @@ double cycle = 0;
 int main(void)
 {
 	DDRD = (1 << PD5) | (1 << PD3);
+	TCCR0 = (1 << COM01) | (1 << WGM01) | (1 << WGM00) | (1 << CS00);
+	PORTB = (1 << PB3);
 	
-	TCCR0 = (1 << COM01) | (1 << WGM01) | (1 << WGM00);
-	TIMSK = (1 << TOIE1);
-	
-	OCR1A = (cycle/100)*255;
-	
-	sei();
-	
-	TCCR1B = (1 << CS10);
-
     while (1) 
     {
 		//_delay_ms(100);
-		
-		PORTD |= (1 << PD5);
-		PORTD &=~ (1 << PD3);
-	 
+	    /* My logic- DOnt use refactoring for now
+	    	OCR1A = (cycle/100)*255;
+	    
 		cycle += 10;
 		if (cycle > 100)
 		{
 			cycle = 0;
-		}
+		}*/
+		
+	    for(int i = 0; i < 255; i++)
+	    {
+		    OCR1A = i;
+		    _delay_ms(500);
+	    }
+	    for(int i = 255; i > 1; i--)
+	    {
+		    OCR1A = i;
+		    _delay_ms(500);
+	    }
     }
-}
-
-ISR(TIMER0_OVF_vect)
-{
-		OCR1A = (cycle/100)*255;
 }
 
